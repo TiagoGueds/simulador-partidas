cd9c28d674076db9f696d978aaeb2c2bdb5dc65e
Tiago_Gueds <guedes.tiago@hotmail.com>
2022-06-26 22:07:40 -0300

    Primeiros passos

--- app/src/main/java/com/example/simulator/MainActivity.kt
+++ app/src/main/java/com/example/simulator/MainActivity.kt
@@ -2,9 +2,19 @@ package com.example.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceSgit statusity_main)
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
